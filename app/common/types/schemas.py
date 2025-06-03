574784a7f32acb4ac75b9864cf0808d1d444d883
Ak <otiosum002@gmail.com>
2025-06-03 20:03:15 +0530

    made some changes ig

--- app/common/types/schemas.py
+++ app/common/types/schemas.py
@@ -1,1 +1,55 @@
-from app.common.types import schemas
+from pydantic import BaseModel
+from typing import List, Optional
+
+
+# Product Schemas
+
+class ProductBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    in_stock: int
+
+class ProductCreate(ProductBase):
+    pass
+
+class Product(ProductBase):
+    id: str
+
+    class Config:
+        orm_mode = True
+
+
+
+# User Schemas
+
+class UserBase(BaseModel):
+    name: str
+    email: str
+
+class UserCreate(UserBase):
+    pass
+
+class User(UserBase):
+    id: str
+
+    class Config:
+        orm_mode = True
+
+
+
+# Order Schemas
+
+class OrderBase(BaseModel):
+    customer_id: str
+    product_ids: List[str]
+
+class OrderCreate(OrderBase):
+    pass
+
+class Order(OrderBase):
+    id: str
+
+    class Config:
+        orm_mode = True
+
